a9efc642ec11cc91409e9ae07440ee07456575c8
Saad Sayyed <saadsyed950@gmail.com>
2024-11-01 19:13:38 +0530

    Slogan and description are done

--- src/_components/Navbar.jsx
+++ src/_components/Navbar.jsx
@@ -1,28 +1,49 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className="w-full bg-gradient-to-b from-black to-blue-950 text-white flex justify-center items-center flex-col lg:gap-y-14 text-center lg:p-10">
       <Link to="/">
-        <h1 className="font-bold text-6xl uppercase tracking-wide font-wildworld">
+        <motion.h1
+          initial={{ y: -10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.4, ease: "easeOut", delay: 0.4 }}
+          className="font-bold text-6xl uppercase tracking-wide font-wildworld"
+        >
           Alacrity.
-        </h1>
+        </motion.h1>
       </Link>
       <div className="flex items-center lg:gap-x-24 font-metana">
         <Link to="/features">
-          <h4 className="font-semibold text-lg uppercase hover:underline hover:underline-offset-4">
+          <motion.h4
+            initial={{ x: -10, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.4, ease: "backOut", delay: 0.6 }}
+            className="font-semibold text-lg uppercase hover:underline hover:underline-offset-4"
+          >
             Features
-          </h4>
+          </motion.h4>
         </Link>
         <Link to="/about">
-          <h4 className="font-semibold text-lg uppercase hover:underline hover:underline-offset-4">
+          <motion.h4
+            initial={{ y: 5, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.4, ease: "backOut", delay: 0.6 }}
+            className="font-semibold text-lg uppercase hover:underline hover:underline-offset-4"
+          >
             About
-          </h4>
+          </motion.h4>
         </Link>
         <Link to="/portfolio">
-          <h4 className="font-semibold text-lg uppercase hover:underline hover:underline-offset-4">
+          <motion.h4
+            initial={{ x: 10, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.4, ease: "backOut", delay: 0.6 }}
+            className="font-semibold text-lg uppercase hover:underline hover:underline-offset-4"
+          >
             Portfolio
-          </h4>
+          </motion.h4>
         </Link>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,28 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full min-h-screen bg-gradient-to-b text-white text-center from-blue-950 to-black flex justify-center items-center flex-col lg:gap-y-12">
+      <div className="flex justify-center items-center flex-col lg:gap-y-8 lg:p-20">
+        <h1 className="lg:text-4xl font-semibold font-wildworld uppercase">
+          Dream Big, Track Better!
+        </h1>
+        <p className="lg:text-base font-metana font-light uppercase">
+          This app helps users stay on track by automating goal progress through
+          visual charts, providing daily affirmations and motivational boosts.
+          With timely reminders, it keeps users aware of their goal deadlines,
+          celebrating achievements and encouraging action on unmet goals to keep
+          them moving forward.
+        </p>
+      </div>
+      <div className="flex items-center lg:gap-x-16">
+        <button className="lg:px-8 lg:py-2 font-metana uppercase lg:text-sm font-semibold bg-white text-blue-950 rounded-full">
+          Sign Up
+        </button>
+        <button className="lg:px-8 lg:py-2 font-metana uppercase lg:text-sm font-semibold bg-transparent border-white border-2 text-white rounded-full">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
