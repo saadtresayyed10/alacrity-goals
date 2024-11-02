618600c0c7e3811528435c9417ecf06547cf706e
Saad Sayyed <saadsyed950@gmail.com>
2024-11-02 23:39:36 +0530

    hero animations done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import SignUp from "../_components/SignUp";
 import Login from "../_components/Login";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const [activeComponent, setActiveComponent] = useState("login");
@@ -24,19 +25,29 @@ const Home = () => {
   return (
     <div className="w-full min-h-screen bg-gradient-to-b text-white text-center from-blue-950 to-black flex justify-center items-center flex-col lg:gap-y-12">
       <div className="flex justify-center items-center flex-col lg:gap-y-8 lg:p-20">
-        <h1 className="lg:text-4xl font-semibold font-wildworld uppercase">
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, ease: "easeOut", delay: 0.8 }}
+          className="lg:text-4xl font-semibold font-wildworld uppercase"
+        >
           Dream Big, Track Better!
-        </h1>
-        <p className="lg:text-base font-metana font-light uppercase">
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, ease: "circOut", delay: 1 }}
+          className="lg:text-base font-metana font-light uppercase"
+        >
           This app helps users stay on track by automating goal progress through
           visual charts, providing daily affirmations and motivational boosts.
           With timely reminders, it keeps users aware of their goal deadlines,
           celebrating achievements and encouraging action on unmet goals to keep
           them moving forward.
-        </p>
+        </motion.p>
       </div>
       <div className="flex items-center lg:gap-x-16">
-        <button
+        <motion.button
           onClick={handleSignUpButton}
           className={`lg:px-8 lg:py-2 font-metana uppercase lg:text-sm font-semibold rounded-full ${
             activeComponent === "signup"
@@ -45,8 +56,8 @@ const Home = () => {
           }`}
         >
           Sign Up
-        </button>
-        <button
+        </motion.button>
+        <motion.button
           onClick={handleLoginButton}
           className={`lg:px-8 lg:py-2 font-metana uppercase lg:text-sm font-semibold rounded-full ${
             activeComponent === "login"
@@ -55,7 +66,7 @@ const Home = () => {
           }`}
         >
           Login
-        </button>
+        </motion.button>
       </div>
       {activeComponent === "login" && <Login goToSignUp={goToSignUp} />}
       {activeComponent === "signup" && <SignUp goToLogin={goToLogin} />}
